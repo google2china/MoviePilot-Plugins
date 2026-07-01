6c2ab3b794d877d314716a5eeff0696aeb0481d2
google2china <tangkissyou@qq.com>
2026-07-01 15:22:40 +0800

    Refine daily_claim_task logic and task info
    Updated task info label and hint for daily_claim_task method. Modified task selection logic based on the current date.

--- plugins/ptautotaskdiy/sites/Tangpt.py
+++ plugins/ptautotaskdiy/sites/Tangpt.py
@@ -32,10 +32,18 @@ class Tasks(BaseTask):
     def __init__(self, cookie: str):
         super().__init__(Tangpt(cookie))
 
-    @task_info(label="躺平任务领取", hint="领取躺平站点的BUG/VIP任务")
+    @task_info(label="躺平任务领取", hint="领取躺平站点的苍蝇腿/VIP任务，平日选苍蝇腿任务，月末选VIP任务。")
     def daily_claim_task(self):
-        task_id_list = ["3", "4"]
-        return "\n".join([self.client.claim_task(item) for item in task_id_list])
-
+        # 获取今天日期，判断是否当月最后一天
+        today = datetime.date.today()
+        _, month_last_day = calendar.monthrange(today.year, today.month)
+        if today.day == month_last_day:
+            task_id = "4"
+        else:
+            task_id = "5"
+        
+        result = self.client.claim_task(task_id)
+        return result
+        
     def daily_checkin(self):
         return self.client.attendance()
